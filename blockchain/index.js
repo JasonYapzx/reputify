5baed9b6656a04e34e83c0429b2f69ebb6756651
JasonYapzx <jasonyapzx@gmail.com>
2024-09-21 06:05:03 +0800

    feat: stake contract work good

--- blockchain/index.js
+++
@@ -1,8 +0,0 @@
-const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
-const client = Client.forTestnet();
-client.setOperator(operatorId, operatorKey);
-
-
-const evmBytecode = await fs.readFile(
-  './trogdor_sol_Trogdor.bin', { encoding: 'utf8' });

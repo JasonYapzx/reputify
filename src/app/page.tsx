a3322596959fc467231659b7e5d71fe220ad1c24
JasonYapzx <jasonyapzx@gmail.com>
2024-09-21 03:21:58 +0800

    feat: add logo + static files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,27 +12,27 @@ import TeamSection from "@/components/layout/sections/team";
 import TestimonialSection from "@/components/layout/sections/testimonial";
 
 export const metadata = {
-  title: "Shadcn - Landing template",
-  description: "Free Shadcn landing page for developers",
+  title: "Reputify",
+  description: "Your reputation tokenized",
   openGraph: {
     type: "website",
     url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    title: "Reputify - Reputation dApp",
+    description: "Your reputation tokenized",
     images: [
       {
         url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
         width: 1200,
         height: 630,
-        alt: "Shadcn - Landing template",
+        alt: "Reputify - Reputation dApp",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     site: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    title: "Reputify - Reputation dApp",
+    description: "Your reputation tokenized",
     images: [
       "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
     ],

20350a49289dae4338e6ffab55208a7ec7b80d35
Jiahua <edures@protonmail.com>
2024-09-21 04:50:51 +0800

    add sql extension test

---
+++ src/app/api/test/sql/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { query } from '../../../../utils/db';
+
+
+
+export async function GET(request: NextRequest) {
+
+    try {
+
+        const int_result = await query("INSERT INTO users (wallet_address, created_at) VALUES ('paoopwie', DEFAULT)");
+
+        let statement = "SELECT * FROM users;";
+
+        const result = await query(statement);
+        
+        return NextResponse.json({ result: result});
+    } catch (err) {
+        console.log('ERROR: API - ', (err as Error).message);
+        const response = {
+            error: (err as Error).message,
+            returnedStatus: 500,
+        }
+        return NextResponse.json(response, { status: 500 });
+    }
+  
+}
---
+++ src/data/import_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users (wallet_address) VALUES ('john.doe@example.com');
+INSERT INTO Users (wallet_address) VALUES ('jane.smith@example.com');

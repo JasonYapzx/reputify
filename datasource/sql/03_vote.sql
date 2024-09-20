596aa8c39c89dd72e008dd63f002741fc95c5ca2
Jiahua <edures@protonmail.com>
2024-09-21 04:50:51 +0800

    add sql extension test

---
+++ datasource/sql/03_vote.sql
@@ -0,0 +1,15 @@
+USE mysql_db;
+
+CREATE TABLE IF NOT EXISTS votes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  post_id INT NOT NULL,
+  user_id INT NOT NULL,
+
+  vote_sign INT NOT NULL, -- 1 for upvotes, -1 for downvote
+  stake_quantity INT NOT NULL,
+
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  
+  FOREIGN KEY (post_id) REFERENCES Posts(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+);
--- datasource/sql/user.sql
+++
@@ -1,9 +0,0 @@
-USE mysql_db;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    wallet_address TEXT NOT NULL,
-    created_at INT NOT NULL,
-    updated_at INT NOT NULL,
-    reputation INT DEFAULT 0
-);

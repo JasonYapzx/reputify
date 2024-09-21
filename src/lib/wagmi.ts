1c1069c7f3f1d725f6390c794f56fb19cb8651fe
JasonYapzx <jasonyapzx@gmail.com>
2024-09-21 14:08:00 +0800

    feat: add account integration with hedera chain

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,13 +1,35 @@
 
 import { http, createConfig } from "wagmi";
+
 import { mainnet } from "wagmi/chains";
 
+import { getOrMapViemChain } from '@dynamic-labs/ethereum-core';
+
+const customEvmNetworks = [
+  {
+    blockExplorerUrls: ['https://hashscan.io/testnet'],
+    chainId: 296,
+    chainName: 'Hedera Testnet',
+    iconUrls: ["https://cryptologos.cc/logos/hedera-hbar-logo.svg?v=035"],
+    name: 'Hedera',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'HBAR',
+      symbol: 'HBAR',
+    },
+    networkId: 296,
+    rpcUrls: ['https://testnet.hashio.io/api'],
+    vanityName: 'Hedera Testnet',
+  },
+]
+
 export const config = createConfig({
-  chains: [mainnet],
+  chains: [mainnet, ...customEvmNetworks.map(getOrMapViemChain)],
   multiInjectedProviderDiscovery: false,
   ssr: true,
   transports: {
-    [mainnet.id]: http(),
+    [mainnet.id]: http('https://testnet.hashio.io/api'),
+    [296]: http(),
   },
 });
 

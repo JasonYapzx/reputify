a1ea32e692cc360667d22df9d9588494553b3829
Eugene Tay <tayyjeugene@gmail.com>
2024-09-21 14:26:01 +0800

    deployed contract

--- blockchain/scripts/deployContract.js
+++ blockchain/scripts/deployContract.js
@@ -18,8 +18,10 @@
  *
  */
 
-import { ethers } from "hardhat";
-import dotenv from "dotenv";
+const { ethers } = require("hardhat");
+const dotenv = require("dotenv");
+const { TokenId, AccountId } = require("@hashgraph/sdk");
+const fs = require("fs");
 
 module.exports = async () => {
   const envFilePath = "../.env";
@@ -34,13 +36,40 @@ module.exports = async () => {
 
   //Using already initialized contract factory object with our contract, we can invoke deploy function to deploy the contract.
   //Accepts constructor parameters from our contract
-  const reputifyTokenAddress = process.env.TOKEN_ID;
-  const ownerAddress = wallet; //TODO: this should be whole calls the function
-  const lockup = await Lockup.deploy(reputifyTokenAddress, ownerAddress); // TODO
+  const reputifyTokenAddress = TokenId.fromString(process.env.TOKEN_ID);
+  const ownerAddress = AccountId.fromString(process.env.STAKING_OPERATOR_ID);
+  const lockup = await Lockup.deploy(
+    reputifyTokenAddress.toSolidityAddress(),
+    ownerAddress.toSolidityAddress()
+  );
 
   //We use wait to receive the transaction (deployment) receipt, which contains contractAddress
   const contractAddress = (await lockup.deployTransaction.wait())
     .contractAddress;
+
+  // update the env file with the new contract ID, token ID, and treasury account ID and private key
+  let envConfig = {};
+  if (fs.existsSync(envFilePath)) {
+    envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+  }
+
+  // Update the necessary key-value pairs
+  // NOTE: Storing treasury key in the env file is not a common practice due to security reasons. Since this is a hackathon, we will momentarily disregard jarring security concerns.
+  envConfig["STAKING_CONTRACT_ID"] = contractAddress;
+
+  // Convert the updated config back to a string
+  const updatedEnvConfig = Object.keys(envConfig)
+    .map((key) => `${key}=${envConfig[key]}`)
+    .join("\n");
+
+  // Write the updated config back to the .env file
+  fs.writeFileSync(envFilePath, updatedEnvConfig, (err) => {
+    if (err) {
+      console.error("Error writing to .env file:", err);
+    } else {
+      console.log(".env file updated successfully");
+    }
+  });
 
   console.log(`Lockup deployed to: ${contractAddress}`);
 
